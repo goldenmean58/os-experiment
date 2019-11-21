c37deec468c9c901e78d3b4ee39e6f9f78db03cd
lishuxiang <lishuxiang@cug.edu.cn>
2019-11-21 14:04:15 +0800

    Add Optimal algorithm

--- exp_five/main.cpp
+++ exp_five/main.cpp
@@ -16,12 +16,14 @@ constexpr int PageSize = _K(1);
 
 constexpr int PageFrameCount = 29;
 
+int address2page(int address) { return address % (PageSize - 1); }
+
 class Scheduler {
 protected:
   std::deque<int> pages;
   std::array<int, PageFrameCount> t;
   int page_lack_time = 0;
-  int address2page(int address) { return address % (PageSize - 1); }
+  int instruction_count = 0;
   virtual std::deque<int>::iterator choose_page_out() = 0;
 
 public:
@@ -30,26 +32,27 @@ public:
       cout << "Address " << address << " is out of memory bound!" << endl;
       return 1; // Out of memory bound
     }
-    int page_index = this->address2page(address);
-    cout << "Visit Page " << page_index << endl;
+    int page_index = address2page(address);
+    // cout << "Visit Page " << page_index << endl;
     std::vector<int> s;
     for (std::size_t i = 0; i < pages.size(); ++i) {
       t[i]++;
     }
     if (pages.end() ==
         std::find(std::begin(pages), std::end(pages), page_index)) {
-      cout << "Page lack!" << endl;
+      // cout << "Page lack!" << endl;
       this->page_lack_time++;
       if (pages.size() >= PageFrameCount) {
         std::deque<int>::iterator page_out = this->choose_page_out();
         int index = std::distance(pages.begin(), page_out);
-        cout << index << endl;
+        // cout << index << endl;
         t[index] = 0;
-        cout << "eliminate page " << index << endl;
+        // cout << "eliminate page " << index << endl;
         pages.erase(page_out);
       }
       pages.push_back(page_index);
     }
+    this->instruction_count++;
     return 0;
   }
   void display() {
@@ -61,14 +64,14 @@ public:
 
 class SchedulerFIFO : public Scheduler {
   std::deque<int>::iterator choose_page_out() final override {
-    cout << "S1" << endl;
+    // cout << "S1" << endl;
     return this->pages.begin();
   }
 };
 
 class SchedulerLRU : public Scheduler {
   std::deque<int>::iterator choose_page_out() final override {
-    cout << "S2" << endl;
+    // cout << "S2" << endl;
     int max_i = 0, max_t = t[0];
     std::deque<int>::iterator it = this->pages.begin();
     for (std::size_t i = 0; i < t.size(); ++i) {
@@ -85,6 +88,34 @@ class SchedulerLRU : public Scheduler {
 public:
 };
 
+class SchedulerOptimal : public Scheduler {
+  std::vector<int> visit_page_index;
+  std::deque<int>::iterator choose_page_out() final override {
+    // cout << "S3" << endl;
+    std::array<int, PageFrameCount> next_step_to_visit = {0};
+    std::deque<int>::iterator it = this->pages.begin();
+    std::array<int, PageFrameCount>::iterator step = next_step_to_visit.begin();
+    for (; it != pages.end() && step != next_step_to_visit.end();
+         ++it, ++step) {
+      *step = 319 - this->instruction_count;
+      for (int i = this->instruction_count; i < 320; ++i) {
+        if (this->visit_page_index[i] == *it) {
+          *step = i - this->instruction_count;
+          break;
+        }
+      }
+    }
+    auto max_ele =
+        std::max_element(next_step_to_visit.begin(), next_step_to_visit.end());
+    int index = std::distance(next_step_to_visit.begin(), max_ele);
+    return this->pages.begin() + index;
+  }
+
+public:
+  SchedulerOptimal(std::vector<int> visit_page_index)
+      : visit_page_index(visit_page_index) {}
+};
+
 int random_integer_in_range(int a, int b) {
   // Return a random integer in [a, b]
   double percent = static_cast<double>(rand()) / RAND_MAX; // [0, RAND_MAX]
@@ -97,34 +128,44 @@ int main(void) {
   SchedulerLRU s2;
   int instruction_count = 0;
   int r = 0;
+  std::vector<int> visit_page_index_track;
   while (true) {
     r = random_integer_in_range(1, _K(32) - 2);
     s1.visit(r);
     s2.visit(r);
+    visit_page_index_track.push_back(address2page(r));
     instruction_count += 2;
     s1.visit(r + 1);
     s2.visit(r + 1);
+    visit_page_index_track.push_back(address2page(r + 1));
     instruction_count += 2;
     r = random_integer_in_range(0, r - 1);
     s1.visit(r);
     s2.visit(r);
+    visit_page_index_track.push_back(address2page(r));
     instruction_count += 2;
     s1.visit(r + 1);
     s2.visit(r + 1);
+    visit_page_index_track.push_back(address2page(r + 1));
     instruction_count += 2;
     r = random_integer_in_range(r + 2, _K(32) - 2);
     s1.visit(r);
     s2.visit(r);
+    visit_page_index_track.push_back(address2page(r));
     instruction_count += 2;
     if (instruction_count >= 320)
       break;
     s1.visit(r + 1);
     s2.visit(r + 1);
+    visit_page_index_track.push_back(address2page(r + 1));
     instruction_count += 2;
   }
-  for (int i = 0; i < 320; i++) {
+  SchedulerOptimal s3(visit_page_index_track);
+  for (auto address : visit_page_index_track) {
+    s3.visit(address);
   }
   s1.display();
   s2.display();
+  s3.display();
   return 0;
 }
